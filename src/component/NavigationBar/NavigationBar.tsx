3da9238a17b770f555e801969743fff0857d5b22
Rakshit Manhas <rakshitmanhas@yahoo.com>
2024-08-12 08:48:38 +0530

    navbar update

--- src/component/NavigationBar/NavigationBar.tsx
+++ src/component/NavigationBar/NavigationBar.tsx
@@ -1,6 +1,6 @@
 import { FunctionComponent } from "react";
 import logo from "/assets/Logo Kagi.png"
-import "./NavBar.css";
+import "./NavigationBar.css";
 
 export type NavigationBarType = {
   className?: string;
