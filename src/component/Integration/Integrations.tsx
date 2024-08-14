37629dde6c92c00026d80638405726e6df4a2351
Rakshit Manhas <rakshitmanhas@yahoo.com>
2024-08-14 09:11:11 +0530

    bug fix

--- src/component/Integration/Integrations.tsx
+++ src/component/Integration/Integrations.tsx
@@ -75,7 +75,7 @@ const Integrations: FunctionComponent<IntegrationsType> = ({ className = "" }) =
           className={styles.vectorBG}
           alt="Vector"
           src="assets/vector-123.svg"
-          srcSet="assets/vector-mobile768.png 768w"
+          srcSet="assets/vector-mobile768.png 768w, assets/vector-123.svg"
           sizes="(max-width: 768px) 768w"
         />
       </div>
